36f8e3049428367c7fd393d5214ed686b15abd1a
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2023-11-15 18:56:50 +0700

    update b1

---
+++ BUOI-1/assets/js/main.js
@@ -0,0 +1,103 @@
+// Hien thi chuong dau tien: Hello 500 ae lop web38
+// console.log("Hello 500 ae lop web38");
+
+// De Khai bao 1 Bien: let, const, var
+// let a = 10;
+// const b = 20;
+// var c = 30;
+// let name;
+// name = "Nguyen Van A";
+
+// In ra gia tri cua bien a
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(name);
+
+
+// Quy tac dat ten bien
+// const lopWeb38 = "FrontEnd";
+// let buoiHocDauTien = "buoi 1";
+
+// console.log(lopWeb38);
+// console.log(buoiHocDauTien);
+
+
+// De kiem tra 1 cai gia tri cua bien: typeof
+// let name = "Nguyen van A";
+// let age = 20; 
+// let email = "example@gmail.com"
+
+// console.log(typeof name);
+// console.log(typeof age);
+
+/*
+  + Pham vi bien
+  -> Global Scope (Bien toan cuc): Nam ngoai function, block
+  -> Function Scope (Bien cuc bo): Nam tronh function
+*/ 
+
+
+// let a = 10;
+
+// Khai bao function ES5
+// function hello() {   // LOGIC TINH TOAN
+//   console.log("hello 500 anh em lop web38");
+//   console.log(a);
+//    b = 20;
+//   // console.log(b);
+// }
+
+// De su dung function
+// hello();
+// console.log(b);
+
+
+// Cac kieu du lieu
+
+// let name = nguyen van a; -> Loi thieu dau nhay don '' or ""
+// let name = 'nguyen van a';
+// console.log(name);
+
+// let email = "nguyenvana@gmail.com";
+// console.log(email);
+
+
+// Mot so chu y voi kieu du lieu string
+// let content = "Nguyen van A. La Hoc vien \"lop Web38\" co so To Hu";
+// let para = 'Nguyen van B. La Hoc vien \'Lop web38\' co so To Huu ';
+
+// console.log(content);
+
+
+// let user = "Tai khoan 'admin'";
+// console.log(user);
+
+
+// Nen dung tranh loi dau o trong kieu du lieu string -- Back-Tick ES6
+// let name = `Hom nay "troi rat" lanh. 'Duong con rat tac' nua!`;
+// console.log(name);
+
+
+// Noi chuoi
+
+// let messege = "Xin chao cac ban." + 
+// " Thoi tiet hom nay bat dau lanh";
+// console.log(messege);
+
+// let name = "Web38";
+// let skill = " FrontEnd";
+
+// let nameSkill = name +skill;
+// console.log(nameSkill);
+
+// ES6 su dung back-tick noi chuoi, bo qua loi ky tu dau nhay (don , kep) long nhau.
+// let a = "Nguyen Van A";
+// let b = "Web 38";
+
+// console.log(`Xin chao "cac ban". Minh ten la ${a} la hoc vien lop ${b}`);
+
+
+// Kieu number
+let numBer = -25.1;
+console.log(numBer);
